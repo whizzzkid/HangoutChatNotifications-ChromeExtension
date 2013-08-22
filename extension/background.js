10ee594d8f160c2f96fb6ae0e15822331caac7bb
whizzzkid <me@nishantarora.in>
2013-08-23 00:08:32 +0530

    Added functionality to raise notifications even if the user is not focused on the current window.

--- extension/background.js
+++ extension/background.js
@@ -1,46 +1,60 @@
-/**********************************************************
-*** FILE	: background.js				***
-*** REV		: 1.6					***
-*** DEV		: me@nishantarora.in		***
-*** DATE	: April 22,2013				***
-*** DESC	: Provides listener function in chrome  ***
-***		  Background and manages notifications	***
-***		  like gmail chat earlier		***
-**********************************************************/
-// setting globals
-notiAct	= false;
-prevUser= "";
-prevTab	= -1;
+/**
+ * @file background.js
+ * @revision 1.7
+ * @author me@nishantarora.in
+ */
 
-// listening for new messages
+/**
+ * Provides listener function in chrome and manages notifications like gmail
+ * chat earlier.
+ */
+
+/**
+ * Setting globals.
+ */
+notiAct  = false;
+prevUser= "";
+prevTab  = -1;
+
+/**
+ * Listening for new messages.
+ */
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	chrome.tabs.query({url: request.url}, function(tabArray) {	
-		if(!tabArray[0].active){
-			notify(request.user.trim(),tabArray[0].id,request.img,request.update);
-		}
-	});
+  chrome.tabs.query({url: request.url}, function(tabArray) {
+    windowID = tabArray[0].windowId;
+    isFocused = true;
+    chrome.windows.get(windowID, function(window){
+      isFocused = (window.focused == "true") ? true : false;
+      if(!isFocused || !tabArray[0].active){
+        console.log(isFocused);
+        notify(request.user.trim(),tabArray[0].id,request.img,request.update);
+      }
+    });
+  });
 });
 
+/**
+ * Create notifications.
+ */
 function notify(user,tabID,img,update){
-	var notiDisp= true;
-	if(user == prevUser && tabID !== prevTab){
-		notiDisp= false;
-	}
-	if(notiDisp){		
-		prevUser=user;
-		prevTab	=tabID;
-		if(notiAct){
-			notification.cancel();
-			notiAct	= false;
-		}
-		notification = webkitNotifications.createNotification(img,user,update);
-		notification.onclick = function(){
-			chrome.tabs.update(tabID, {active: true});
-			this.cancel();
-			notiAct	= false;
-		};
-		notification.show();
-		notiAct	= true;
-	}
-	//console.log({notiAct: notiAct, prevUser: prevUser, prevTab: prevTab, user: user, tabID: tabID, notiDisp: notiDisp});
+  var notiDisp= true;
+  if(user == prevUser && tabID !== prevTab){
+    notiDisp= false;
+  }
+  if(notiDisp){
+    prevUser=user;
+    prevTab  =tabID;
+    if(notiAct){
+      notification.cancel();
+      notiAct = false;
+    }
+    notification = webkitNotifications.createNotification(img,user,update);
+    notification.onclick = function(){
+      chrome.tabs.update(tabID, {active: true});
+      this.cancel();
+      notiAct = false;
+    };
+    notification.show();
+    notiAct = true;
+  }
 }
--- extension/tool/smuggler.js
+++ extension/tool/smuggler.js
@@ -1,50 +1,67 @@
-/**********************************************************
-*** FILE	: smuggler.js				***
-*** REV		: 1.7					***
-*** DEV		: me@nishantarora.in		***
-*** DATE	: April 18,2013				***
-*** DESC	: Smuggles data from chat frame to the	***
-***		  document, so that a notification can	***
-***		  created.				***
-**********************************************************/
-//setting a proper timeout
+/**
+ * @file smuggler.js
+ * @rev 1.8
+ * @author me@nishantarora.in
+ */
+
+/**
+ * Smuggles data from chat frame to the document, so that a notification can be
+ * created.
+ */
+
+/**
+ * Setting a proper timeout.
+ */
 $(document).ready(function(){
-	setTimeout(function(){}, 5000);
-	setInterval(check_new_message, 100);	
-	n= $("div").length;
-	window_focus = false;	
-	$(window).focus(function() {
-		window_focus = true;
-	}).blur(function() {
-		window_focus = false;
-	});
+  setTimeout(function(){}, 5000);
+  setInterval(check_new_message, 100);
+  n= $("div").length;
+  window_focus = false;
+  $(window).focus(function() {
+    window_focus = true;
+  }).blur(function() {
+    window_focus = false;
+  });
 });
-//counting difference in divs
+
+/**
+ * Counting difference in divs.
+ */
 n=-1;
 divno=-1;
 function check_new_message(){
-	if(n==-1){
-		n= $("div").length;
-	}
-	divno	= $("div").length;
-	if(divno>n && n!==0){
-		if(!window_focus){
-			update_notifier();
-		}
-		n=divno;
-	}
+  if(n==-1){
+    n= $("div").length;
+  }
+  divno  = $("div").length;
+  if(divno>n && n!==0){
+    if(!window_focus){
+      update_notifier();
+    }
+    n=divno;
+  }
 }
-//updating the notification service running in background.js
+/**
+ * Updating the notification service running in background.js
+ */
 function update_notifier(){
-	// thumbnail
-	var allThumbs	= $('img').filter(function(){
-		return ($(this).width() ==32) && ($(this).height() ==32)
-	});
-	var reqThumb	= allThumbs[allThumbs.length-1].src;
-	//message
-	var message	= $('div.Mu.SP:last').text();
-	//user
-	var user	= $('div.UR.UG:last').text();
-	console.log({img: reqThumb, user: user,update: message,url: document.referrer});
-	chrome.extension.sendRequest({img: reqThumb, user: user,update: message,url: document.referrer});
+
+  /**
+   * Thumbnail.
+   */
+  var allThumbs  = $('img').filter(function(){
+    return ($(this).width() ==32) && ($(this).height() ==32)
+  });
+  var reqThumb  = allThumbs[allThumbs.length-1].src;
+
+  /**
+   * Message.
+   */
+  var message  = $('div.Mu.SP:last').text();
+
+  /**
+   * User.
+   */
+  var user  = $('div.UR.UG:last').text();
+  chrome.extension.sendRequest({img: reqThumb, user: user,update: message,url: document.referrer});
 }
